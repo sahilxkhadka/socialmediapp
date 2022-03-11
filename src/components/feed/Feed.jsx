f96bce8b0d34acdcf3a1f2c58122b4a83c4ad9a6
stormraider2059 <sahilkhadka767@gmail.com>
2022-03-11 22:42:37 +0545

    post section

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Post from '../post/Post'
 import Share from '../share/Share'
 import "./feed.css"
 
@@ -7,6 +8,7 @@ export default function Feed() {
     <div className='feed'>
       <div className="feedWrapper">
         <Share />
+        <Post />
       </div>
     </div>
   )
