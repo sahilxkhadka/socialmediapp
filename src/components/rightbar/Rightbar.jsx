c5ec5e90cf806a0ea09a0c2cb6a05cbaa1820071
stormraider2059 <sahilkhadka767@gmail.com>
2022-03-15 15:16:38 +0545

    rightbar added

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -2,9 +2,27 @@ import React from 'react'
 import "./rightbar.css"
 
 export default function Rightbar() {
-  return (
-    <div className='rightbar'>
-      Rightbar
-    </div>
-  )
+	return (
+		<div className='rightbar'>
+			<div className="rightbarWrapper">
+				<div className="birthdayContainer">
+					<img className='birthdayImg' src="assets/gift.png" alt="" />
+					<span className="birthdayText">
+						<b>Sandesh</b> and <b>3 other friends</b> have birthday today.
+					</span>
+				</div>
+				<img src="assets/ad.png" alt="" className="rightbarAd" />
+				<h4 className="rightbarTitle">Online Friends</h4>
+				<ul className="rightbarFriendList">
+					<li className="rightbarFriend">
+						<div className="rightbarProfileImgContainer">
+							<img src="assets/person/3.jpeg" alt="" className="rightbarProfileImg" />
+							<span className='rightbarOnline'></span>
+						</div>
+						<span className="rightbarUsername">Aruna Tamang</span>
+					</li>
+				</ul>
+			</div>
+		</div>
+	)
 }
